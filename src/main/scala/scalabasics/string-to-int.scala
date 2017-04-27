92adcb647a1b920722ffa4fdae333d837792b09b
Keith Cissell <kcissell@keith-cis-mac.local>
2017-04-27 16:25:51 -0500

    Changed vars to vals, used case classes, cleaned up code

--- src/main/scala/scalabasics/string-to-int.scala
+++ src/main/scala/scalabasics/string-to-int.scala
@@ -7,22 +7,16 @@ object StringToInt {
   def main(args: Array[String]) = {
     val strs: List[String] = List("We","are","","a","list",null,"of","strings")
     println(s"Strings: $strs")
-    val ints = getInts(strs)
-    println(s"Ints:\t $ints")
-  }
-
-  def getInts(strs: List[String]): List[Int] = {
+    // Translate strings to ints
     val ints = for {
       s <- strs
       // pattern match the string
       val len = s match {
         case null => 0
-        case "" => -1
-        case _ => s.length
+        case ""   => -1
+        case _    => s.length
       }
     } yield len
-    // return the list of ints
-    return ints
+    println(s"Ints:\t $ints")
   }
-
 }
